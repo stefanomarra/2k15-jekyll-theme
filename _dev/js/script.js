b05598ac5a7aa620af3e8582cd2e9045ac5f9b6a
Stefano Marra <stefano.marra1987@gmail.com>
2015-07-14 22:17:51 +0200

    default page layouts added and index homepage added

---
+++ _dev/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function($){
+	// browser window scroll (in pixels) after which the "menu" link is shown
+	var offset = 300;
+
+	var navigationContainer = $('#site-nav'),
+		mainNavigation = navigationContainer.find('#site-primary-nav ul');
+
+	//hide or show the "menu" link
+	checkMenu();
+	$(window).scroll(function(){
+		checkMenu();
+	});
+
+	//open or close the menu clicking on the bottom "menu" link
+	$('.site-nav-trigger').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('menu-is-open');
+		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+
+	});
+
+	function checkMenu() {
+		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+			navigationContainer.addClass('is-fixed').find('.site-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+				mainNavigation.addClass('has-transitions');
+			});
+		} else if ($(window).scrollTop() <= offset) {
+			//check if the menu is open when scrolling up
+			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+				//close the menu with animation
+				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					//wait for the menu to be closed and do the rest
+					mainNavigation.removeClass('is-visible is-hidden has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.site-nav-trigger').removeClass('menu-is-open');
+				});
+			//check if the menu is open when scrolling up - fallback if transitions are not supported
+			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.site-nav-trigger').removeClass('menu-is-open');
+			//scrolling up with menu closed
+			} else {
+				navigationContainer.removeClass('is-fixed');
+				mainNavigation.removeClass('has-transitions');
+			}
+		}
+	}
+
+	$('.post-sidebar').stick_in_parent({
+		parent: ".post-content"
+	});
+
+	$('.post-sidebar').on('sticky_kit:bottom', function(e) {
+		$(this).parent().css('position', 'static');
+	})
+	.on('sticky_kit:unbottom', function(e) {
+		$(this).parent().css('position', 'relative');
+	});
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,72 @@
+// Include Gulp
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({camelize: true});
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Styles
+gulp.task('styles', function() {
+	return gulp.src('./_dev/less/style.less')
+		.pipe(plugins.less())
+		.pipe(plugins.autoprefixer('last 2 versions'))
+		.pipe(gulp.dest('./assets/css/build'))
+		.pipe(plugins.rename({suffix: '.min'}))
+		.pipe(plugins.minifyCss())
+		.pipe(gulp.dest('./assets/css/build'))
+		.pipe(gulp.dest('./_site/assets/css/build'))
+		.pipe(plugins.notify({message: 'Styles task complete'}))
+		.pipe(plugins.filter('**/*.css')) // Filtering stream to only css files
+        .pipe(browserSync.reload({stream:true}));
 });
+
+gulp.task('scripts', function() {
+	return gulp.src('./_dev/js/script.js')
+		.pipe(plugins.concat('themes.js'))
+		.pipe(gulp.dest('./assets/js/build'))
+		.pipe(plugins.rename({suffix: '.min'}))
+		.pipe(plugins.uglify())
+		.pipe(gulp.dest('./assets/js/build'))
+		.pipe(gulp.dest('./_site/assets/js/build'))
+		.pipe(plugins.notify({message: 'Scripts task complete'}));
+});
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify('Building Jekyll');
+    var spawn = require('child_process').spawn;
+    return spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browsersync', ['jekyll-build'], function() {
+    browserSync.init({
+        server: {
+            baseDir: '_site'
+        },
+        // port: 4000,
+        host: "localhost"
+    });
+});
+
+// Watch
+gulp.task('watch', function() {
+
+	// Watch .less files
+	gulp.watch('./_dev/less/*.less', ['styles']);
+
+	// Watch .js files
+	gulp.watch('./_dev/js/*.js', ['scripts']);
+
+	// Watch .htm files
+	gulp.watch(['_config.yml', 'index.html', '_includes/*.html', '_layouts/*.html', '*.md', '_posts/*'], ['jekyll-rebuild']);
+
+});
+
+gulp.task('default', ['styles', 'scripts', 'browsersync', 'watch']);
